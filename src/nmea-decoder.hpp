9eb7e330b4b8f03ab886f30717f8bf048583ab88
olbender <olbender@users.noreply.github.com>
2018-11-22 21:26:19 +0100

    Seems ok, but does not yet work with the AP. Data goes through.

--- src/nmea-decoder.hpp
+++ src/nmea-decoder.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Ola Benderius
+ * Copyright (C) 2018  Christian Berger
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,18 +18,43 @@
 #ifndef NMEA_DECODER
 #define NMEA_DECODER
 
-class NmeaDecoder {
-  public:
-    NmeaDecoder() noexcept;
-    ~NmeaDecoder() = default;
-    NmeaDecoder(const NmeaDecoder &) = delete;
-    NmeaDecoder(NmeaDecoder &&) = delete;
-    NmeaDecoder &operator=(const NmeaDecoder &) = delete;
-    NmeaDecoder &operator=(NmeaDecoder &&) = delete;
+#include "nmea-decoder-constants.hpp"
 
-    void decode(const std::string &) noexcept;
+#include <chrono>
+#include <functional>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <utility>
 
-    //   private:
+class NMEADecoder {
+   private:
+    NMEADecoder(const NMEADecoder &) = delete;
+    NMEADecoder(NMEADecoder &&)      = delete;
+    NMEADecoder &operator=(const NMEADecoder &) = delete;
+    NMEADecoder &operator=(NMEADecoder &&) = delete;
+
+   public:
+    NMEADecoder(std::function<void(const double &latitude, const double &longitude, const std::chrono::system_clock::time_point &tp)> delegateLatitudeLongitude,
+                std::function<void(const float &heading, const std::chrono::system_clock::time_point &tp)> delegateHeading) noexcept;
+    ~NMEADecoder() = default;
+
+   public:
+    void decode(const std::string &data, std::chrono::system_clock::time_point &&tp) noexcept;
+
+   private:
+    std::function<void(const double &latitude, const double &longitude, const std::chrono::system_clock::time_point &tp)> m_delegateLatitudeLongitude{};
+    std::function<void(const float &heading, const std::chrono::system_clock::time_point &tp)> m_delegateHeading{};
+
+    bool m_foundHeader{false};
+    bool m_foundCRLF{false};
+    bool m_buffering{false};
+    uint32_t m_toRemove{0};
+    std::stringstream m_buffer{};
+    uint32_t m_arrayWriteIndex{0};
+    std::array<char, static_cast<uint32_t>(NMEADecoderConstants::NMEA_BUFFER)> m_bufferForNextNMEAMessage{};
+    NMEADecoderConstants m_nextNMEAMessage{NMEADecoderConstants::UNKNOWN};
 };
 
 #endif
+
--- src/opendlv-device-autopilot-nmea.cpp
+++ src/opendlv-device-autopilot-nmea.cpp
@@ -21,15 +21,38 @@
 #include <sstream>
 #include <string>
 #include <thread>
+#include <chrono>
 
 
 #include "cluon-complete.hpp"
 #include "opendlv-standard-message-set.hpp"
 
-#include "serial-port.hpp"
+#include "serialport.hpp"
 
 #include "nmea-decoder.hpp"
 #include "nmea-encoder.hpp"
+
+
+std::string appendChecksum(std::string const &message)
+{
+  char const *s = message.c_str();
+  int32_t c = 0;
+
+  while (*s) {
+    c ^= *s++;
+  }
+
+  std::stringstream stream;
+  stream << "$" << message << "*" << std::hex << c;
+
+  std::string tmp = stream.str();
+  std::transform(tmp.begin(), tmp.end(), tmp.begin(), ::toupper);
+
+  std::string nmea = tmp + static_cast<char>(13) + static_cast<char>(10);
+
+  return nmea;
+}
+
 
 int32_t main(int32_t argc, char **argv) {
   int32_t retCode{0};
@@ -41,8 +64,8 @@ int32_t main(int32_t argc, char **argv) {
     retCode = 1;
   } else {
     uint16_t const CID{static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
-   // uint32_t const ID{(commandlineArguments["id"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
-    uint32_t const BAUDRATE{(commandlineArguments["nmea-baudrate"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["nmea-baudrate"])) : 0};
+    // uint32_t const ID{(commandlineArguments["id"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
+    uint32_t const BAUDRATE{(commandlineArguments["nmea-baudrate"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["nmea-baudrate"])) : 4800};
     bool const VERBOSE{commandlineArguments.count("verbose") != 0};
     float const FREQ = std::stof(commandlineArguments["freq"]);
     std::string const PORT(commandlineArguments["nmea-port"]);
@@ -57,16 +80,47 @@ int32_t main(int32_t argc, char **argv) {
     serial::Port serialPort(PORT, BAUDRATE, serial::mode::ReadWrite);
     serialPort.read(serialReceived);
 
+
+
+
+
+
+
+    cluon::TCPConnection fromDevice{"10.42.0.206", 9999,
+      [&serialPort](std::string &&d, std::chrono::system_clock::time_point &&tp) noexcept {
+        std::cout << d << std::endl;
+        serialPort.write(std::move(d));
+      },
+      [&argv](){ std::cerr << "[" << argv[0] << "] Connection lost." << std::endl; exit(1); }
+    };
+
+
+
+
+
+
+
+
+
+
+
     cluon::OD4Session od4{CID};
 
     auto atFrequency{[&od4, &VERBOSE, &serialPort]() -> bool
       {
-        std::string message = "Hello world";
-
+        std::string message = "GPAPB,A,A,0.000,L,N,V,V,065.8,T,WP1,065.8,T,065.8,T";
+        message = appendChecksum(message);
         if (VERBOSE) {
           std::cout << "Sending message: " << message << std::endl;
         }
-        
+        serialPort.write(message);
+
+
+        message = "GPRMB,A,0.000,L,WP0,WP1,5741.5449,N,01154.7963,E,0.3,065.8,0.05,V";
+        message = appendChecksum(message);
+        if (VERBOSE) {
+          std::cout << "Sending message: " << message << std::endl;
+        }
         serialPort.write(message);
 
         return true;
@@ -76,3 +130,4 @@ int32_t main(int32_t argc, char **argv) {
   }
   return retCode;
 }
+
